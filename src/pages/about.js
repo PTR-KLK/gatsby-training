8a28c2f846f5076cc48f11f776ce940383cf4276
Piotr Kielak <p.kielak@outlook.com>
2020-07-16 18:41:10 +0200

    initial configuration

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>About Simple Cat Site</h1>
+      <p>
+        Cat ipsum dolor sit amet, your pillow is now my pet bed for ooooh
+        feather moving feather! inspect anything brought into the house for why
+        use post when this sofa is here. Sleep nap i’m so hungry i’m so hungry
+        but ew not for that making bread on the bathrobe. Sleep on keyboard claw
+        at curtains stretch and yawn nibble on tuna ignore human bite human hand
+        but cats are the world or hiss and stare at nothing then run suddenly
+        away i like big cats and i can not lie.
+      </p>
+    </Layout>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>Cats are amazing</h1>
+      <div>
+        <img
+          src="https://placekitten.com/600/400"
+          alt="Group of pandas eating bamboo"
+        />
+      </div>
+    </Layout>
+  )
 }
