e267191bea16af9ec4799fd9f673749fa99cd655
Piotr Kielak <p.kielak@outlook.com>
2020-07-28 01:59:44 +0200

    multiple image query in component, navbar styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,25 @@
 import React from "react"
 import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import { PageLink } from "./pageLink"
 
 export default function Header() {
-  const { fileName } = useStaticQuery(
+  const { ...data } = useStaticQuery(
     graphql`
       query {
-        fileName: file(relativePath: { eq: "images/grass.jpg" }) {
+        grass: file(relativePath: { eq: "images/grass.jpg" }) {
           childImageSharp {
             fluid(maxWidth: 2560) {
-              ...GatsbyImageSharpFluid_tracedSVG
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        cat: file(relativePath: { eq: "images/cat.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 256, maxHeight: 256) {
+              ...GatsbyImageSharpFluid
             }
           }
         }
@@ -24,28 +32,30 @@ export default function Header() {
       <nav
         css={css`
           display: flex;
-          align-items: baseline;
+          align-items: center;
+          justify-content: space-between;
           width: 100%;
-          position: fixed;
+          padding: ${rhythm(0.125)} ${rhythm(1)} ${rhythm(0.125)};
+          position: sticky;
           z-index: 3;
           color: #fff;
-          background: green;
+          background: #697c17;
         `}
       >
         <PageLink to={`/`}>
-          <h3
+          <Img
             css={css`
-              margin: 0;
+              width: ${rhythm(1.75)};
+              height: ${rhythm(1.75)};
+              border-radius: ${rhythm(0.25)};
             `}
-          >
-            Simple Cat Site
-          </h3>
+            loading="eager"
+            fluid={data.cat.childImageSharp.fluid}
+            alt="Cat staring at you"
+          />
         </PageLink>
-        <section
-          css={css`
-            margin: 0 0 0 auto;
-          `}
-        >
+        <section>
+          <PageLink to={`/`}>Blog</PageLink>{" "}
           <PageLink to={`/portfolio/`}>Portfolio</PageLink>{" "}
           <PageLink to={`/about/`}>About</PageLink>
         </section>
@@ -60,12 +70,13 @@ export default function Header() {
             width: 100%;
             height: 33vh;
             z-index: 1;
-            box-shadow: none, 0 1px 2px rgba(0,0,0,0.24) inset;
+            box-shadow: none, 0 1px 2px rgba(0, 0, 0, 0.24) inset;
+            backgroundcolor: #697c17;
           `}
           imgStyle={{ objectFit: "cover" }}
           loading="eager"
-          fluid={fileName.childImageSharp.fluid}
-          alt="Fat cat"
+          fluid={data.grass.childImageSharp.fluid}
+          alt="Grass"
         />
       </figure>
     </header>
